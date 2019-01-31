30567ef9060a96bd7041169c635ebc19daeeb487
Rodolfo Araujo <rodoufu@gmail.com>
2019-01-31 21:06:08 -0200

    SpringBoot project

---
+++ src/main/java/com/github/rodoufu/currencyvalues/CurrencyValuesApplication.java
@@ -0,0 +1,13 @@
+package com.github.rodoufu.currencyvalues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyValuesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyValuesApplication.class, args);
+	}
+
+}
