b6e0f67a7c9eaff294156c122f61fedea6a05acf
rodoufu <rodoufu@gmail.com>
2019-02-01 11:31:12 -0200

    Unit testing.

--- src/main/java/com/github/rodoufu/currencyvalues/dao/BitcoinPriceDAO.java
+++ src/main/java/com/github/rodoufu/currencyvalues/dao/BitcoinPriceDAO.java
@@ -1,6 +1,8 @@
 package com.github.rodoufu.currencyvalues.dao;
 
 import com.github.rodoufu.currencyvalues.entity.BitcoinPrice;
+
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -16,4 +18,7 @@ public interface BitcoinPriceDAO extends CrudRepository<BitcoinPrice, Long> {
     List<BitcoinPrice> findTop10ByOrderByDateDesc();
     /** The top 10 Bitcoin prices by Id, use primary key index. */
     List<BitcoinPrice> findTop10ByOrderByIdDesc();
+    
+    @Modifying
+    void deleteAll();
 }
--- src/test/java/com/github/rodoufu/currencyvalues/BitcoinPriceJobTest.java
+++ src/test/java/com/github/rodoufu/currencyvalues/BitcoinPriceJobTest.java
@@ -1,66 +1,118 @@
 package com.github.rodoufu.currencyvalues;
 
-import com.github.rodoufu.currencyvalues.bean.CryptoTicker;
-import com.github.rodoufu.currencyvalues.bean.FiatTicker;
-import com.github.rodoufu.currencyvalues.entity.BitcoinPrice;
-import com.github.rodoufu.currencyvalues.job.BitcoinPriceJob;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doCallRealMethod;
+import static org.mockito.Mockito.when;
 
 import java.math.BigDecimal;
-import java.util.Collection;
+import java.net.URISyntaxException;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestClientException;
+
+import com.github.rodoufu.currencyvalues.bean.CryptoTicker;
+import com.github.rodoufu.currencyvalues.bean.FiatTicker;
+import com.github.rodoufu.currencyvalues.dao.BitcoinPriceDAO;
+import com.github.rodoufu.currencyvalues.entity.BitcoinPrice;
+import com.github.rodoufu.currencyvalues.job.BitcoinPriceJob;
 
 @RunWith(SpringRunner.class)
+//@DataJpaTest
 @SpringBootTest
 public class BitcoinPriceJobTest {
-
 	@Autowired
+	private BitcoinPriceDAO bitcoinPriceDAO;
+	
+	@MockBean
 	private BitcoinPriceJob bitcoinPriceJob;
+	@MockBean
+	private TickerData tickerData;
 
 	private final BigDecimal error = new BigDecimal(1e-5);
 
-	@Test
-	public void emptyFiatPrices() {
-		final FiatTicker fiatTicker = new FiatTicker();
-		final CryptoTicker cryptoTicker = new CryptoTicker();
-		cryptoTicker.setBuy(new BigDecimal(1));
-
-		final Collection<BitcoinPrice> bitcoinPrices = bitcoinPriceJob.calculateBitcoinPrice(cryptoTicker, fiatTicker);
-		Assert.assertNotNull(bitcoinPrices);
-		Assert.assertEquals(0, bitcoinPrices.size());
+	@Before
+	public void setUp() {
+		doAnswer((invocation) -> tickerData).when(bitcoinPriceJob).getTickerData();
+		doAnswer((invocation) -> bitcoinPriceDAO).when(bitcoinPriceJob).getBitcoinPriceDAO();
+		bitcoinPriceDAO.deleteAll();
 	}
 
 	@Test
-	public void oneFiatPrice() {
+	public void emptyFiatPricesJob() throws URISyntaxException, RestClientException, InterruptedException, ExecutionException {
 		final FiatTicker fiatTicker = new FiatTicker();
-		fiatTicker.getRates().put("BRL", new BigDecimal(1));
+		when(tickerData.getFiatTicker()).thenReturn(CompletableFuture.completedFuture(fiatTicker));
+
 		final CryptoTicker cryptoTicker = new CryptoTicker();
 		cryptoTicker.setBuy(new BigDecimal(2));
+		when(tickerData.getCryptoTicker()).thenReturn(CompletableFuture.completedFuture(cryptoTicker));
 
-		final List<BitcoinPrice> bitcoinPrices = bitcoinPriceJob.calculateBitcoinPrice(cryptoTicker, fiatTicker);
-		Assert.assertNotNull(bitcoinPrices);
-		Assert.assertEquals(1, bitcoinPrices.size());
-		Assert.assertTrue(new BigDecimal(1).subtract(bitcoinPrices.get(0).getValue()).compareTo(error) < 0);
+		when(bitcoinPriceJob.calculateBitcoinPrice(cryptoTicker, fiatTicker)).thenCallRealMethod();
+		doCallRealMethod().when(bitcoinPriceJob).saveBitcoinPrice(ArgumentMatchers.anyList());
+		doCallRealMethod().when(bitcoinPriceJob).scheduleFetchAndSaveBitcoinPrice();
+
+		bitcoinPriceJob.scheduleFetchAndSaveBitcoinPrice();
+		final List<BitcoinPrice> top10bitcoinPrice = bitcoinPriceDAO.findTop10ByOrderByIdDesc();
+		Assert.assertNotNull(top10bitcoinPrice);
+		Assert.assertEquals(0, top10bitcoinPrice.size());
 	}
 
 	@Test
-	public void twoFiatPrices() {
+	public void oneFiatPriceJob() throws URISyntaxException, RestClientException, InterruptedException, ExecutionException {
 		final FiatTicker fiatTicker = new FiatTicker();
 		fiatTicker.getRates().put("BRL", new BigDecimal(1));
+		when(tickerData.getFiatTicker()).thenReturn(CompletableFuture.completedFuture(fiatTicker));
+
+		final CryptoTicker cryptoTicker = new CryptoTicker();
+		cryptoTicker.setBuy(new BigDecimal(2));
+		when(tickerData.getCryptoTicker()).thenReturn(CompletableFuture.completedFuture(cryptoTicker));
+
+		when(bitcoinPriceJob.calculateBitcoinPrice(cryptoTicker, fiatTicker)).thenCallRealMethod();
+		doCallRealMethod().when(bitcoinPriceJob).saveBitcoinPrice(ArgumentMatchers.anyList());
+		doCallRealMethod().when(bitcoinPriceJob).scheduleFetchAndSaveBitcoinPrice();
+
+		bitcoinPriceJob.scheduleFetchAndSaveBitcoinPrice();
+		final List<BitcoinPrice> top10bitcoinPrice = bitcoinPriceDAO.findTop10ByOrderByIdDesc();
+		Assert.assertNotNull(top10bitcoinPrice);
+		Assert.assertEquals(1, top10bitcoinPrice.size());
+		Assert.assertTrue(new BigDecimal(2).subtract(top10bitcoinPrice.get(0).getValue()).compareTo(error) < 0);
+	}
+
+	@Test
+	public void twoFiatPricesJob() throws URISyntaxException, RestClientException, InterruptedException, ExecutionException {
+		final FiatTicker fiatTicker = new FiatTicker();
+		// Just changing the Map implementation because in this case I want to keep the order the elements are inserted.
+		fiatTicker.setRates(new LinkedHashMap<>());
+		fiatTicker.getRates().put("BRL", new BigDecimal(1));
 		fiatTicker.getRates().put("USD", new BigDecimal(.25));
+		when(tickerData.getFiatTicker()).thenReturn(CompletableFuture.completedFuture(fiatTicker));
+
 		final CryptoTicker cryptoTicker = new CryptoTicker();
 		cryptoTicker.setBuy(new BigDecimal(2000));
+		when(tickerData.getCryptoTicker()).thenReturn(CompletableFuture.completedFuture(cryptoTicker));
 
-		final List<BitcoinPrice> bitcoinPrices = bitcoinPriceJob.calculateBitcoinPrice(cryptoTicker, fiatTicker);
-		Assert.assertNotNull(bitcoinPrices);
-		Assert.assertEquals(1, bitcoinPrices.size());
-		Assert.assertTrue(new BigDecimal(2000).subtract(bitcoinPrices.get(0).getValue()).compareTo(error) < 0);
-		Assert.assertTrue(new BigDecimal(500).subtract(bitcoinPrices.get(0).getValue()).compareTo(error) < 0);
+		when(bitcoinPriceJob.calculateBitcoinPrice(cryptoTicker, fiatTicker)).thenCallRealMethod();
+		doCallRealMethod().when(bitcoinPriceJob).saveBitcoinPrice(ArgumentMatchers.anyList());
+		doCallRealMethod().when(bitcoinPriceJob).scheduleFetchAndSaveBitcoinPrice();
+
+		bitcoinPriceJob.scheduleFetchAndSaveBitcoinPrice();
+		final List<BitcoinPrice> top10bitcoinPrice = bitcoinPriceDAO.findTop10ByOrderByIdDesc();
+		Assert.assertNotNull(top10bitcoinPrice);
+		Assert.assertEquals(2, top10bitcoinPrice.size());
+		// Descending order
+		Assert.assertTrue(new BigDecimal(500).subtract(top10bitcoinPrice.get(0).getValue()).compareTo(error) < 0);
+		Assert.assertTrue(new BigDecimal(2000).subtract(top10bitcoinPrice.get(1).getValue()).compareTo(error) < 0);
 	}
 
 }
