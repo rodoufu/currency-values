9196ffc8fc868c78dfd1bb05330268b3a725e305
rodoufu <rodoufu@gmail.com>
2019-02-01 21:57:13 -0200

    Solving REST API problem caused by HTTP 403 code.

--- src/main/java/com/github/rodoufu/currencyvalues/CurrencyValuesApplication.java
+++ src/main/java/com/github/rodoufu/currencyvalues/CurrencyValuesApplication.java
@@ -3,20 +3,34 @@ package com.github.rodoufu.currencyvalues;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableScheduling
 public class CurrencyValuesApplication {
 
-	@Bean
-	RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(CurrencyValuesApplication.class, args);
-	}
+    @Bean
+    HttpEntity<String> entity() {
+        final HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.add("user-agent",
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
+        return new HttpEntity<>("parameters", headers);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyValuesApplication.class, args);
+    }
 
 }
