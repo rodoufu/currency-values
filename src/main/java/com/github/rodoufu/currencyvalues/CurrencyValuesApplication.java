c1f508963c58fee066ca29df9b20eee8438a8084
Rodolfo Araujo <rodoufu@gmail.com>
2019-01-31 23:34:50 -0200

    Application.

--- src/main/java/com/github/rodoufu/currencyvalues/CurrencyValuesApplication.java
+++ src/main/java/com/github/rodoufu/currencyvalues/CurrencyValuesApplication.java
@@ -2,9 +2,18 @@ package com.github.rodoufu.currencyvalues;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableScheduling
 public class CurrencyValuesApplication {
+
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyValuesApplication.class, args);
